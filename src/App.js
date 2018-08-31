add58f1b4cb794fb9cb73fd0ffd3ca7e443c790f
Raymond Woods <ray@MacBook-Pro.local>
2018-08-30 20:15:17 -0700

    checkpoint 2: * setup Firebase reference * listing chatrooms * defined initial styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC-tiVW9ponHmrLdJw6KItM9CrxrmZwxUw",
+  authDomain: "bloc-chat-51882.firebaseapp.com",
+  databaseURL: "https://bloc-chat-51882.firebaseio.com",
+  projectId: "bloc-chat-51882",
+  storageBucket: "bloc-chat-51882.appspot.com",
+  messagingSenderId: "915139954508"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Bloc Chat</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="App-room-content">
+          <RoomList firebase={firebase}></RoomList>
+          <section className="App-room-chat-area">
+            <p>test content</p>
+            <p>test content</p>
+            <p>test content</p>
+          </section>
+        </section>
       </div>
     );
   }
